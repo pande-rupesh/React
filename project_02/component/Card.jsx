5869c47f9a2cb688ed028926d9adabbcdfd90fc1
pande-rupesh <rupeshpande2001@gmail.com>
2026-04-14 16:50:28 +0530

    Component

---
+++ project_02/component/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div className='card'>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, impedit?</p></div>
+    </>
+  )
+}
+
+export default Card
---
+++ project_02/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../component/card'
+
+const App = () => {
+  return (
+    <>
+    <Card/>
+    </>
+  )
+}
+
+export default App
